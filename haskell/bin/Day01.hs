83a70569af496dd7c03dd5a65675620484635f6f
cybai <cyb.ai.815@gmail.com>
2021-12-02 01:12:59 +0900

    Finish Day 1 Part 2

--- haskell/bin/Day01.hs
+++ haskell/bin/Day01.hs
@@ -7,22 +7,33 @@ import AdventOfCode.Utils
 newtype Day01 = Day01 Integer
   deriving (Show)
 
-instance Solutions Day01 where
-  part1 input = Day01 $ go 0 depths
-    where
-      depths = (\n -> read n :: Integer) <$> lines input
+depths :: String -> [Integer]
+depths x = (\n -> read n :: Integer) <$> lines x
 
+increment :: Integer -> Integer -> (Integer -> Integer)
+increment x y = if x < y then (+ 1) else (+ 0)
+
+instance Solutions Day01 where
+  part1 input = Day01 $ go 0 (depths input)
+    where
       go :: Integer -> [Integer] -> Integer
       go count (x : y : []) = increment x y $ count
       go count (x : y : xs) = go (increment x y $ count) (y : xs)
       go count _ = count
 
-      increment :: Integer -> Integer -> (Integer -> Integer)
-      increment x y = if x < y then (+ 1) else (+ 0)
-
-  part2 = undefined
+  part2 input = Day01 $ go 0 0 Nothing (depths input)
+    where
+      go :: Integer -> Integer -> Maybe Integer -> [Integer] -> Integer
+      go count _ Nothing (_ : _ : _ : []) = count
+      go count _ Nothing (x : y : z : xs) = go count (x + y + z) (Just x) (y : z : xs)
+      go count prevSum (Just n) (_ : _ : z : []) = increment prevSum (prevSum - n + z) $ count
+      go count prevSum (Just n) (x : y : z : xs) = go (increment prevSum next $ count) next (Just x) (y : z : xs)
+        where
+          next = prevSum - n + z
+      go count _ _ _ = count
 
 main :: IO ()
 main = do
   input <- readInput 1
   putStrLn $ show (part1 @Day01 input)
+  putStrLn $ show (part2 @Day01 input)
