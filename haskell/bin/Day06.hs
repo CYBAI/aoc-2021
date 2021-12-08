0fa7209474235e32e3d616371890988fa7df6638
cybai <cyb.ai.815@gmail.com>
2021-12-09 00:19:22 +0900

    Polish Day6 to complete part 2

--- haskell/bin/Day06.hs
+++ haskell/bin/Day06.hs
@@ -1,24 +1,28 @@
 import AdventOfCode.Utils
 import Data.List.Split (splitOn)
+import qualified Data.Map.Strict as M
 
 parseInput :: String -> [Integer]
 parseInput = fmap strToInt . splitOn ","
 
-calc :: Integer -> Integer
-calc 0 = 6
-calc n = n - 1
+next :: Integer -> Integer -> [Integer] -> [Integer]
+next _ _ [] = error "Unexpected empty array"
+next 0 c (x : xs) = x + c : xs
+next n c (x : xs) = x : next (n - 1) c xs
 
-part1 :: String -> Integer
-part1 = go 0 . parseInput
+solve :: Integer -> [Integer] -> Integer
+solve 0 xs = sum xs
+solve n xs = solve (n - 1) (go xs)
   where
-    go :: Integer -> [Integer] -> Integer
-    go 80 nums = fromIntegral $ length nums
-    go d nums = go (d + 1) (next ++ replicate zeros 8)
-      where
-        next = calc <$> nums
-        zeros = length $ filter (== 0) nums
+    go :: [Integer] -> [Integer]
+    go [] = error "Unexpected empty array"
+    go (x : xs) = next 6 x xs ++ [x]
 
 main :: IO ()
 main = do
   input <- readInput 6
-  print (part1 input)
+  let nums = genNums input
+  print (solve 80 nums)
+  print (solve 256 nums)
+  where
+    genNums = M.elems . foldr (M.alter ((+ 1) <$>)) (M.fromList $ zip [0 .. 8] (replicate 9 0)) . parseInput
