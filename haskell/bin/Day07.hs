60045f755ce5baf1218bb7872ff1ecae48ab8881
cybai <cyb.ai.815@gmail.com>
2021-12-11 21:34:50 +0900

    Complete Day 7 Part 2

--- haskell/bin/Day07.hs
+++ haskell/bin/Day07.hs
@@ -15,17 +15,38 @@ collect = go M.empty
     inc Nothing = Just 1
     inc (Just x) = Just (x + 1)
 
-part1 :: [Integer] -> Integer
-part1 xs = minimum $ map (\n -> M.foldlWithKey (calc n) 0 count) (M.keys count)
+fuel :: (Integer -> Integer -> Integer -> Integer) -> M.Map Integer Integer -> Integer -> Integer
+fuel op hm n = M.foldlWithKey go 0 hm
   where
-    count = collect xs
+    go :: Integer -> Integer -> Integer -> Integer
+    go acc k _ | n == k = acc
+    go acc k c = acc + op n k c
 
-    calc :: Integer -> Integer -> Integer -> Integer -> Integer
-    calc n acc k _ | n == k = acc
-    calc n acc k c = acc + (abs (n - k) * c)
+calculate :: (Integer -> Integer -> Integer -> Integer) -> M.Map Integer Integer -> [Integer]
+calculate op hm = fuel op hm <$> [minB .. maxB]
+  where
+    minB = case M.lookupMin hm of
+      Just (x, _) -> x
+      Nothing -> error "Unexpected no minimum value"
+    maxB = case M.lookupMax hm of
+      Just (x, _) -> x
+      Nothing -> error "Unexpected no maximum value"
+
+part1 :: [Integer] -> Integer
+part1 = minimum . calculate solve . collect
+  where
+    solve n k c = abs (n - k) * c
+
+part2 :: [Integer] -> Integer
+part2 = minimum . calculate solve . collect
+  where
+    solve n k c = ((1 + r) * r `div` 2) * c
+      where
+        r = abs (n - k)
 
 main :: IO ()
 main = do
   input <- readInput 7
   let nums = parseInput input
   print (part1 nums)
+  print (part2 nums)
