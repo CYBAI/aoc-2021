3a58ac063a9cf89f484852b1bd62a88c9f8cfcca
cybai <cyb.ai.815@gmail.com>
2021-12-18 17:52:19 +0900

    Complete Day 8 Part 2

--- haskell/bin/Day08.hs
+++ haskell/bin/Day08.hs
@@ -1,4 +1,8 @@
 import AdventOfCode.Utils
+import Data.List (foldl', sort)
+import qualified Data.Map.Strict as M
+import Data.Set (Set, (\\))
+import qualified Data.Set as S
 import qualified Text.Parsec as P
 import Text.Parsec.String (Parser)
 
@@ -34,7 +38,76 @@ part1 = sum . (length . filter checkLen . snd <$>) . parse
       7 -> True -- 8
       _ -> False
 
+transform :: [String] -> M.Map Int (Set (Set Char))
+transform = go M.empty
+  where
+    go hm [] = hm
+    go hm (x : xs) = go (M.alter f (length x) hm) xs
+      where
+        f Nothing = Just $ S.singleton (S.fromList x)
+        f (Just s) = Just $ S.insert (S.fromList x) s
+
+decrypt :: M.Map Int (Set (Set Char)) -> [Char]
+decrypt hm = [zero, one, two, three, four, five, six]
+  where
+    first = S.elemAt 0
+
+    zero = first $ digitSeven \\ digitOne
+    four = first $ digitEight \\ digitNine
+    six = first $ first $ S.filter ((1 ==) . S.size) $ S.map (\\ noBottomDigitNine) (hm M.! 6)
+    (two, three) =
+      let (stwo, sthree) = S.partition (\c -> S.member c digitOne) $ S.map (first . (digitEight \\)) $ S.delete digitNine (hm M.! 6)
+       in (first stwo, first sthree)
+    five = first $ digitOne \\ S.singleton two
+    one = first $ digitFour \\ (S.fromList [two, three, five])
+
+    noBottomDigitNine = S.insert zero (first $ hm M.! 4)
+
+    digitOne = first (hm M.! 2)
+    digitFour = first (hm M.! 4)
+    digitSeven = first (hm M.! 3)
+    digitEight = first (hm M.! 7)
+    digitNine = S.insert six noBottomDigitNine
+
+toDigit :: [Integer] -> Integer
+toDigit [0, 1, 2, 4, 5, 6] = 0
+toDigit [2, 5] = 1
+toDigit [0, 2, 3, 4, 6] = 2
+toDigit [0, 2, 3, 5, 6] = 3
+toDigit [1, 2, 3, 5] = 4
+toDigit [0, 1, 3, 5, 6] = 5
+toDigit [0, 1, 3, 4, 5, 6] = 6
+toDigit [0, 2, 5] = 7
+toDigit [0, 1, 2, 3, 4, 5, 6] = 8
+toDigit [0, 1, 2, 3, 5, 6] = 9
+toDigit _ = error "Not supported digit"
+
+findPos :: [Char] -> [String] -> [Integer]
+findPos _ [] = []
+findPos cs (str : xs) = go str : (findPos cs xs)
+  where
+    go s = toDigit $ sort (charIndex <$> s)
+
+    indexes :: M.Map Char Integer
+    indexes = f M.empty 0 cs
+      where
+        f :: M.Map Char Integer -> Integer -> [Char] -> M.Map Char Integer
+        f hm _ [] = hm
+        f hm i (cc : cs') = f (M.insert cc i hm) (i + 1) cs'
+
+    charIndex :: Char -> Integer
+    charIndex c = case M.lookup c indexes of
+      Just x -> x
+      Nothing -> error "Unexpected character"
+
+solve :: Line -> Integer
+solve (pats, outputs) = strToInt $ foldl' (++) "" (show <$> findPos (decrypt $ transform pats) outputs)
+
+part2 :: String -> Integer
+part2 input = sum $ solve <$> parse input
+
 main :: IO ()
 main = do
   input <- readInput 8
   print (part1 input)
+  print (part2 input)
